2c51fe5ce30104a046c97832335a28a5da798405
Fatma Belhaj <fatmazahra.benhajminiaoui@esprit.tn>
2023-05-06 12:51:35 +0100

    login

---
+++ src/tn/esprit/jobtopia/gui/LoginForm.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tn.esprit.jobtopia.gui;
+
+import com.codename1.io.ConnectionRequest;
+import com.codename1.ui.Button;
+import com.codename1.ui.Container;
+import com.codename1.ui.Dialog;
+import com.codename1.ui.Form;
+import com.codename1.ui.TextField;
+import com.codename1.ui.layouts.BoxLayout;
+import com.codename1.ui.layouts.FlowLayout;
+import java.io.IOException;
+
+import tn.esprit.jobtopia.entities.User;
+import tn.esprit.jobtopia.services.ServiceUser;
+
+/**
+ *
+ * @author Administrateur
+ */
+public class LoginForm extends Form{
+ public LoginForm() {
+        setTitle("Login Form");
+        setLayout(new FlowLayout(CENTER, CENTER));
+        
+        TextField tfUsername = new TextField("", "Email here...");
+        TextField tfPwd = new TextField("", "Password here...", 0, TextField.PASSWORD);
+        Button btnLogin = new Button("Se connecter");
+        Container cn = new Container(BoxLayout.y());
+        
+        btnLogin.addActionListener(e->{
+            ServiceUser su= new ServiceUser();
+            try {
+                User user= su.login(tfUsername.getText(), tfPwd.getText());
+                if(user.getId()!=0){
+                    
+                    if(user.getRole().equals("CLIENT")){
+                        
+                        new ListFreelancerForm().show();
+                    } 
+                else {
+                     Dialog.show("Warning", "You are a freelancer ! ", "OK", null);
+                    
+                        }
+                }
+                           else {
+                     Dialog.show("Warning", "Invalid username or password ! ", "OK", null);
+                    
+                        }                 
+                    
+            } catch (IOException ex) {
+               // Logger.getLogger(LoginForm.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        });
+        
+        cn.addAll(tfUsername, tfPwd, btnLogin);
+        add(cn);        
+    }
+    
+    
+    
+}
--- src/tn/esprit/jobtopia/services/ServiceUser.java
+++ src/tn/esprit/jobtopia/services/ServiceUser.java
@@ -5,10 +5,134 @@
  */
 package tn.esprit.jobtopia.services;
 
+import com.codename1.io.CharArrayReader;
+import com.codename1.io.ConnectionRequest;
+import com.codename1.io.JSONParser;
+import com.codename1.io.NetworkEvent;
+import com.codename1.io.NetworkManager;
+import com.codename1.ui.events.ActionListener;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import tn.esprit.jobtopia.entities.Freelancer;
+import tn.esprit.jobtopia.entities.User;
+import tn.esprit.jobtopia.gui.ListFreelancerForm;
+
 /**
  *
  * @author Administrateur
  */
 public class ServiceUser {
-    
+     public static ServiceUser instance = null;
+    public boolean resultOK;
+    private ConnectionRequest req;
+
+    public ServiceUser() {
+        req = new ConnectionRequest();
+    }
+
+    public static ServiceUser getInstance() {
+        if (instance == null) {
+            instance = new ServiceUser();
+        }
+        return instance;
+    }
+
+    public User login(String username,String password) throws IOException {
+      String url = "http://127.0.0.1:8000/UserJson/" +username+ "/" +password;
+                  System.out.println(url);
+
+req.setUrl(url);
+req.setPost(false);
+ User user = new User();
+req.addResponseListener(new ActionListener<NetworkEvent>() {
+    @Override
+    public void actionPerformed(NetworkEvent evt) {
+        String json = new String(req.getResponseData());
+       
+     if(json.equals("invalid")){
+         User user= new User();
+          System.out.println("invalid");
+     }
+     else{
+        JSONParser parser = new JSONParser();
+        try {
+            Map<String, Object> userMap = parser.parseJSON(new CharArrayReader(json.toCharArray()));
+            String nom = (String) userMap.get("nom");
+            String prenom = (String) userMap.get("prenom");
+            String email = (String) userMap.get("email");
+            String role = (String) userMap.get("role");
+           
+         float id = Float.parseFloat(userMap.get("id").toString());
+            String photoData = (String) userMap.get("imagepath");
+            user.setId((int)id);
+            user.setRole(role);
+           user.setNom(nom);
+           user.setPrenom(prenom);
+           user.setEmail(email);
+
+            System.out.println(user);
+          //  float salaire = Float.parseFloat((String) freelancerMap.get("salaire"));
+
+            
+            // do something with the freelancer object
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+     }
+        req.removeResponseListener(this);
+    }
+});
+NetworkManager.getInstance().addToQueueAndWait(req);
+
+        return user;
+    }
+
+    public ArrayList<Freelancer> getAllTasks() {
+        String url = "http://127.0.0.1:8000/listFreelancersJson";
+        req.setUrl(url);
+        req.setPost(false);
+        req.addResponseListener(new ActionListener<NetworkEvent>() {
+            @Override
+            public void actionPerformed(NetworkEvent evt) {
+                ArrayList<Freelancer> tasks = parseFreelancer(new String(req.getResponseData()));
+                req.removeResponseListener(this);
+            }
+        });
+        NetworkManager.getInstance().addToQueueAndWait(req);
+        ArrayList<Freelancer> tasks = parseFreelancer(new String(req.getResponseData()));
+
+        return tasks;
+    }
+
+    public ArrayList<Freelancer> parseFreelancer(String jsonText) {
+        ArrayList<Freelancer> freelancers = new ArrayList<>();
+
+        try {
+            JSONParser j = new JSONParser();
+            Map<String, Object> freeListJson
+                    = j.parseJSON(new CharArrayReader(jsonText.toCharArray()));
+            System.out.println(freeListJson);
+            List<Map<String, Object>> list = (List<Map<String, Object>>) freeListJson.get("root");
+            for (Map<String, Object> obj : list) {
+                Freelancer t = new Freelancer();
+                t.setImagePath(obj.get("imagepath").toString());
+
+                float id = Float.parseFloat(obj.get("id").toString());
+                // t.setStatus(((int) Float.parseFloat(obj.get("status").toString())));
+                t.setId((int) id);
+                t.setNom(obj.get("nom").toString());
+                t.setPrenom(obj.get("prenom").toString());
+                t.setCategorie(obj.get("categorie").toString());
+
+                freelancers.add(t);
+            }
+
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return freelancers;
+    }
+
 }
